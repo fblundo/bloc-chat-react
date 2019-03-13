f8c320340ce1fa0a870a67823b879622ac678552
fblundo <federico.blundo@gmail.com>
2019-03-13 00:43:46 +0000

    list-room-assignment-completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList'; //importing the component
 import './App.css';
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCfQbekmSDdTxv9b80tGP0toGXQ912eof0",
+    authDomain: "my-project-1547733005314.firebaseapp.com",
+    databaseURL: "https://my-project-1547733005314.firebaseio.com",
+    projectId: "my-project-1547733005314",
+    storageBucket: "my-project-1547733005314.appspot.com",
+    messagingSenderId: "542568497244"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+           <RoomList firebase={firebase}  name={this.name} />
         </header>
       </div>
     );
