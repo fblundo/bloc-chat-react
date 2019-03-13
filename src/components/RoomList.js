5bb9550402d4a2faaa6e19bcca1c44fcf43e5726
fblundo <federico.blundo@gmail.com>
2019-03-13 15:30:45 +0000

    list-rooms-assignment-reviewed

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -11,9 +11,10 @@ class RoomList extends Component {
 
   componentDidMount() {
     this.roomsRef.on('child_added', snapshot => {
-       const room = snapshot.val();
-       room.key = snapshot.key;
-       this.setState({ rooms: this.state.rooms.concat( room ) })
+
+      const room = snapshot.val();
+      room.key = snapshot.key;
+      this.setState({ rooms: this.state.rooms.concat( room ) })
      });
   }
 
